bf87b6e4b18e2aeb6783bf636fa2d31b1d434a05
LAPTOP-1Q2QCVLF\rafa <rafael.sanchez.salado@gmail.com>
2020-12-09 21:12:09 +0100

    Creating the Spring project for the backend

---
+++ soundwave-spring/src/test/java/com/soundwave/soundwavespring/SoundwaveSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soundwave.soundwavespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundwaveSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
